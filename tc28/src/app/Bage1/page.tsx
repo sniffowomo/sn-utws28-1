700dae8372afd0ebd375e1f1a0b0395153a45b3d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 20:45:09 +0000

    🎱

--- tc28/src/app/Bage1/page.tsx
+++ tc28/src/app/Bage1/page.tsx
@@ -1,5 +1,7 @@
+import "./bage.css";
+
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return <div className="text-9xl p-4 animate-bounce">page</div>;
 }
