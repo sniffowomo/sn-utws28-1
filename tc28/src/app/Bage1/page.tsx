f2bd957c4e06822825b24044898fe06ca9bc1d0d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 20:54:56 +0000

    🎱

--- tc28/src/app/Bage1/page.tsx
+++ tc28/src/app/Bage1/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import "./bage.css";
 
 import React from "react";
 
 export default function page() {
-  return <div className="text-9xl p-4 animate-bounce">page</div>;
+  return (
+    <div>
+      <div className="flex justify-center">
+        <Link href="/">
+          <button
+            type="button"
+            className=" text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-lg px-14 py-3 text-center mr-2 mb-2 ">
+            Lime
+          </button>
+        </Link>
+      </div>
+      <div>
+        <div className="text-9xl p-4 animate-bounce container text-center">
+          page
+        </div>
+      </div>
+    </div>
+  );
 }
