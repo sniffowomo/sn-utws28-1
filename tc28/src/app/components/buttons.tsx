3fddb58af0d8280538090b58d6adfdc70e62041b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 02:00:27 +0000

    🎱

---
+++ tc28/src/app/components/buttons.tsx
@@ -0,0 +1,10 @@
+export default function Butts() {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <button className="rounded-full bg-blue-500 p-4">Save Changes</button>
+      <button className="rounded-full bg-green-500 p-4">Save Changes</button>
+      <button className="rounded-full bg-pink-500 p-4">Save Changes</button>
+      <button className="rounded-full bg-teal-500 p-4">Save Changes</button>
+    </div>
+  );
+}
