cb74346f8ff558dedb1451b3e5733c2f5b08add8
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 02:13:15 +0000

    🎱

--- tc28/src/app/components/buttons.tsx
+++ tc28/src/app/components/buttons.tsx
@@ -1,7 +1,14 @@
+"use client";
+import Link from "next/link";
+
+
+
 export default function Butts() {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <button className="rounded-full bg-blue-500 p-4">Save Changes</button>
+    <div className="flex flex-col items-center gap-4 p-8">
+      <Link href="/Bage1">
+        <button className="rounded-full bg-blue-500 p-4">Save Changes</button>
+      </Link>
       <button className="rounded-full bg-green-500 p-4">Save Changes</button>
       <button className="rounded-full bg-pink-500 p-4">Save Changes</button>
       <button className="rounded-full bg-teal-500 p-4">Save Changes</button>
