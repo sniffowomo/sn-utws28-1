dfa27c99b2c54dc94f14d7f12850b239df22bc39
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 01:29:59 +0000

    🚇

--- sn/g.sh
+++ sn/g.sh
@@ -3,7 +3,7 @@ echo "==================="
 echo ""
 echo "git add -all && git commit -m <addurshit>&& git push"
 git add --all 
-git commit -m "(^///^)"
+git commit -m "🚇"
 git push --progress
 echo ""
 echo "=========PUSH DONE========="
