7d9e032edd2d1c656acdb56ab8f6230bffcdb2c9
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-21 02:23:03 +0000

    🎱

--- tc28/src/app/components/buttons.tsx
+++ tc28/src/app/components/buttons.tsx
@@ -1,17 +1,23 @@
 "use client";
 import Link from "next/link";
 
-
-
 export default function Butts() {
   return (
     <div className="flex flex-col items-center gap-4 p-8">
       <Link href="/Bage1">
-        <button className="rounded-full bg-blue-500 p-4">Save Changes</button>
+        <button className="rounded-full bg-blue-500 p-4 hover:bg-black hover:border-2 hover:border-blue-500 hover:font-bold hover:text-blue-300">
+          Save Changes
+        </button>
       </Link>
-      <button className="rounded-full bg-green-500 p-4">Save Changes</button>
-      <button className="rounded-full bg-pink-500 p-4">Save Changes</button>
-      <button className="rounded-full bg-teal-500 p-4">Save Changes</button>
+      <button className="rounded-full bg-green-500 p-4 hover:bg-black hover:border-2 hover:border-green-500 hover:font-bold">
+        Save Changes
+      </button>
+      <button className="rounded-full bg-pink-500 p-4  hover:bg-black hover:border-2 hover:border-pink-500 hover:font-bold">
+        Save Changes
+      </button>
+      <button className="rounded-full bg-teal-500 p-4  hover:bg-black hover:border-2 hover:border-teal-500 hover:font-bold">
+        Save Changes
+      </button>
     </div>
   );
 }
