cee89a9c0b93e8befbfe718e66c383dc5adb41c9
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-22 01:00:41 +0000

    🎱

--- tc28/src/app/components/buttons.tsx
+++ tc28/src/app/components/buttons.tsx
@@ -1,34 +1,40 @@
 "use client";
+
 import { Oswald } from "next/font/google";
 import Link from "next/link";
+import { Zoom } from "react-awesome-reveal";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 export default function Butts() {
   return (
-    <div className={oswald.className}>
-      <div className="flex flex-grow-0 flex-col items-center gap-4 p-8 ">
-        <Link href="/Bage1">
-          <button className="rounded-full bg-blue-500 p-4 hover:bg-black hover:border-2 hover:border-blue-500 hover:font-bold hover:text-blue-300 hover:cursor-help">
-            Save Changes
-          </button>
-        </Link>
-        <Link href="/">
-          <button className="rounded-full bg-green-500 p-4 hover:bg-black hover:border-2 hover:border-green-500 hover:font-bold hover:text-green-300 hover:cursor-help">
-            Save Changes
-          </button>
-        </Link>
-        <Link href="/">
-          <button className="rounded-full bg-pink-500 p-4  hover:bg-black hover:border-2 hover:border-pink-500 hover:font-bold hover:text-pink-300 hover:cursor-help">
-            Save Changes
-          </button>
-        </Link>
-        <Link href="/">
-          <button className="rounded-full bg-teal-500 p-4  hover:bg-black hover:border-2 hover:border-teal-500 hover:font-bold hover:text-teal-300 hover:cursor-help">
-            Save Changes
-          </button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <Zoom cascade={true} damping={0.1} delay={100}>
+        <div className={oswald.className}>
+          <div className="flex flex-grow-0 flex-col items-center gap-4 p-8 ">
+            <Link href="/Bage1">
+              <button className="rounded-full bg-blue-500 p-4 hover:bg-black hover:border-2 hover:border-blue-500 hover:font-bold hover:text-blue-300 hover:cursor-help">
+                Save Changes
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="rounded-full bg-green-500 p-4 hover:bg-black hover:border-2 hover:border-green-500 hover:font-bold hover:text-green-300 hover:cursor-help">
+                Save Changes
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="rounded-full bg-pink-500 p-4  hover:bg-black hover:border-2 hover:border-pink-500 hover:font-bold hover:text-pink-300 hover:cursor-help">
+                Save Changes
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="rounded-full bg-teal-500 p-4  hover:bg-black hover:border-2 hover:border-teal-500 hover:font-bold hover:text-teal-300 hover:cursor-help">
+                Save Changes
+              </button>
+            </Link>
+          </div>
+        </div>
+      </Zoom>
+    </>
   );
 }
