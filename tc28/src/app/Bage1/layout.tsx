e0ba4f3174e27477c32378b6003e918a7603e89d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-22 01:23:32 +0000

    🎱

--- tc28/src/app/Bage1/layout.tsx
+++ tc28/src/app/Bage1/layout.tsx
@@ -9,8 +9,7 @@ export default function BageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section
-      className={`${dansing.className} bg-purple-800 mx-auto min-h-screen`}>
+    <section className={`${dansing.className}  mx-auto min-h-screen`}>
       {/* Include shared UI here e.g. a header or sidebar */}
       {children}
     </section>
