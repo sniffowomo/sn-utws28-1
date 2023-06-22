5cd0f4624821e19f4bb0bbed24904e2615a2fcf9
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-22 01:37:54 +0000

    🎱

--- tc28/src/app/page.tsx
+++ tc28/src/app/page.tsx
@@ -4,7 +4,7 @@ import Butts from "./components/buttons";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className=" text-center text-5xl bg-gradient-to-tr from-indigo-800 via-purple-800 to-pink-800 text-transparent bg-clip-text hover:bg-gradient-to-tl hover:from-red-800 hover:via-blue-800 hover:to-green-800 hover:font-bold py-6">
         <h1>Chapter 23 Work</h1>
       </div>
