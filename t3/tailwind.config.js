c266de4df2364e899b7aa8b22654e69dfc01e9cc
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-23 19:47:55 +0000

    🎱

--- t3/tailwind.config.js
+++ t3/tailwind.config.js
@@ -12,7 +12,30 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '300% 300%',
+            'background-position': 'left center',
+          },
+          '25%': {
+            'background-size': '500% 500%',
+            'background-position': 'right center',
+          },
+          '50%': {
+            'background-size': '800% 800%',
+            'background-position': 'right top',
+          },
+          '75%': {
+            'background-size': '200% 200%',
+            'background-position': 'left top',
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 }
