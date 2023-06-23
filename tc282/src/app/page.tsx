203ebc3fbdb00ddd7e5107ea495362c4406d1a9c
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-23 02:20:43 +0000

    🎱

--- tc282/src/app/page.tsx
+++ tc282/src/app/page.tsx
@@ -3,12 +3,12 @@ import Buts from "./components/buts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen mx-auto flex-col items-center justify-center text-center bg-[url('https://raw.githubusercontent.com/sniffowomo/sn-utws28-1/w1/tc282/src/app/components/bd.gif')] bg-fixed bg-center">
-      <h1 className="text-5xl bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-extrabold hover:bg-gradient-to-br hover:from-orange-500 hover:via-red-500 hover:to-blue-500 mb-10">
+    <main className="min-h-screen flex flex-col items-center justify-center text-center bg-[url('https://raw.githubusercontent.com/sniffowomo/sn-utws28-1/w1/tc282/src/app/components/bd.gif')] bg-fixed bg-center ">
+      <h1 className="text-6xl bg-gradient-to-r from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-transparent animate-gradient font-extrabold mb-10 bg-400%  ">
         Ch28: Tailind Mini Projects
       </h1>
       <div className="border-y-2 border-red-500 py-5">
-        <p className="mt-3 text-yellow-500">
+        <p className="mt-3 text-yellow-500]">
           Tailwind mini projects click button below
         </p>
       </div>
