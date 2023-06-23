8abfcbfde2be08b32f5d730b41876fda8c2af75d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-23 22:06:22 +0000

    🎱

--- t3/src/app/Fram/page.tsx
+++ t3/src/app/Fram/page.tsx
@@ -6,7 +6,10 @@ export default function Fram() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[url('https://media4.giphy.com/media/m9ed5fyVIyCaaZuc43/giphy.gif')] bg-cover bg-center ">
       <motion.div
         animate={{ rotate: 720 }}
-        transition={{ ease: 'easeOut', duration: 20 }}>
+        transition={{ type: 'spring', duration: 20 }}
+        whileHover={{ scale: 1.2 }}
+        onHoverStart={(e) => {}}
+        onHoverEnd={(e) => {}}>
         <h1 className="text-8xl">Dance Dance</h1>
       </motion.div>
     </main>
