07f5eba8169d8816df671aa199e6d8ec6e646b23
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-23 00:39:32 +0000

    🎱

--- tc282/src/app/components/buts.tsx
+++ tc282/src/app/components/buts.tsx
@@ -13,7 +13,7 @@ export default function Buts() {
         <div className="flex flex-col items-center justify-center p-5 gap-3">
           <button
             type="button"
-            className="text-white mx-8 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
+            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
             Blue
           </button>
           <button
