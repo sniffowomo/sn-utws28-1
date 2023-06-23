2a04795607eb9f0a5180c5e715449297394ae2f1
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-23 20:00:47 +0000

    🎱

--- t3/src/app/page.tsx
+++ t3/src/app/page.tsx
@@ -1,31 +1,13 @@
-'use client'
-
 import Image from 'next/image'
+import Bu from './components/bu'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <div>
-          <h1 className="text-8xl font-bold text-center bg-clip-text text-transparent bg-[url('https://media1.giphy.com/media/NsBknNwmmWE8WU1q2U/giphy.gif')] animate-text ">
-            Tailwind Animate Testing
-          </h1>
-        </div>
-        <div>
-          <p className="p-5 text-center animate-fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            reiciendis!
-          </p>
-          <p className="p-5 text-center animate-fade animate-delay-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            reiciendis!
-          </p>
-          <p className="p-5 text-center animate-jump-in">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            reiciendis!
-          </p>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-8xl font-bold text-center bg-clip-text text-transparent bg-[url('https://media1.giphy.com/media/NsBknNwmmWE8WU1q2U/giphy.gif')] animate-text ">
+        Tailwind Animate Testing
+      </h1>
+      <Bu />
     </main>
   )
 }
