d1f4f99a6ca7b550f78e29a89f4de2eb31f20a01
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-24 02:10:36 +0000

    🎱

--- vbg/src/app/page.tsx
+++ vbg/src/app/page.tsx
@@ -5,7 +5,7 @@ import Vid from './components/vid'
 export default function Home() {
   return (
     <main>
-      <div className="sticky flex flex-col h-screen items-center justify-center text-center z-50 ">
+      <div className="absolute flex flex-col h-screen items-center justify-center text-center z-50 ">
         <h1 className="text-9xl">Hey</h1>
         <p className=" text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
