d874eff1af52752549d51979c539ae138f1c0c25
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-24 00:51:05 +0000

    🎱

---
+++ vbg/src/app/components/vid.tsx
@@ -0,0 +1,7 @@
+export default function Vid() {
+  return (
+    <div>
+      <video src="v.mp4" autoPlay loop muted />
+    </div>
+  )
+}
--- vbg/tailwind.config.js
+++ vbg/tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 4s ease infinite alternate',
+      },
     },
   },
   plugins: [],
